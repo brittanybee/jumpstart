8e2fe0a41215ee7567677653cae636cdddb64883
brittanybee <bmbruce13@gmail.com>
2017-08-07 15:40:13 -0400

    js complete

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -8,3 +8,14 @@ $('.carousel').slick({
   pauseOnFocus: false,
   pauseonHover: false,
 });
+
+var timeScroll = 0
+
+var userScroll = $(timeScroll).scrollTop();
+
+if (userScroll - timeScroll > 50){
+  var navHeight = $('.navbar').css('height');
+}
+
+$(".navbar").animate({top: '-' + navHeight}, 150);
+timeScroll=userScroll
