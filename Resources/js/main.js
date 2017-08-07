0a74842edab0d5bddb8b1652822c98a6bc4c63a3
brittanybee <bmbruce13@gmail.com>
2017-08-07 15:18:46 -0400

    js

---
+++ Resources/js/main.js
@@ -0,0 +1,24 @@
+$('.single-item').slick();
+
+$('.autoplay').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+
+$('.arrows').slick({
+  arrows: false
+});
+
+$('.draggable').slick({
+  draggable: false
+});
+
+$('.pauseOnFocus').slick({
+  pauseOnFocus: false
+});
+
+$('.pauseOnHover').slick({
+pauseOnHover: false
+});
