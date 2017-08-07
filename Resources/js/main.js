67cd4a2ef20d719cd8f9b56e90129774a76c2e15
brittanybee <bmbruce13@gmail.com>
2017-08-07 15:26:53 -0400

    js3

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,24 +1,10 @@
-$('.single-item').slick();
-
-$('.autoplay').slick({
+$('.carousel').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 4000,
-});
-
-$('.arrows').slick({
-  arrows: false
-});
-
-$('.draggable').slick({
-  draggable: false
-});
-
-$('.pauseOnFocus').slick({
-  pauseOnFocus: false
-});
-
-$('.pauseOnHover').slick({
-pauseOnHover: false
+  arrows: false,
+  draggable: false,
+  pauseOnFocus: false,
+  pauseonHover: false,
 });
